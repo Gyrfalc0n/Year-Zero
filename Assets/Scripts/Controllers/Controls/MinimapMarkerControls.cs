f54f2650996e3387fb3597de401c8febac4aa161
Axel Ribon <axel.ribon@epita.fr>
2019-01-22 22:22:19 +0100

    0.0.4.6
    -Marker Added (clickable on minimap and world space)
    -Marker synced with teammates

--- Assets/Scripts/Controllers/Controls/MinimapMarkerControls.cs
+++ Assets/Scripts/Controllers/Controls/MinimapMarkerControls.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
 
 public class MinimapMarkerControls : PlayerControls
 {
     [SerializeField]
-    Minimap minimap;
+    Transform minimapPanel;
+    [SerializeField]
+    Marker markerPrefab;
 
     public override void Update()
     {
@@ -24,7 +28,40 @@ public class MinimapMarkerControls : PlayerControls
 
     public override void LeftClick()
     {
-        //put balise
+        Vector3 pos = Vector3.zero;
+        bool mark = false;
+        if (GetComponent<MovementControls>().MouseOnMinimap())
+        { 
+            pos = Input.mousePosition;
+            mark = true;
+        }
+        else if (!GetComponent<MovementControls>().MouseOverUI())
+        {
+            pos = GetComponent<MovementControls>().WorldSpaceToMinimap();
+            mark = true;
+        }
+        if (mark)
+        {
+            CreateMarker(pos);
+            if (!PhotonNetwork.OfflineMode)
+            {
+                foreach (Player player in PhotonNetwork.PlayerList)
+                {
+                    Hashtable tmp = player.CustomProperties;
+                    if ((int)tmp["Team"] == InstanceManager.instanceManager.GetTeam() && player != PhotonNetwork.LocalPlayer)
+                    {
+                        photonView.RPC("CreateMarker", player, pos);
+                    }
+                }
+            }
+        }
         Cancel();
+    }
+
+    [PunRPC]
+    public void CreateMarker(Vector3 pos)
+    {
+        Marker tmp = Instantiate(markerPrefab, minimapPanel);
+        tmp.Init(pos);
     }
 }
