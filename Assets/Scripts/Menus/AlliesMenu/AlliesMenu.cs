17161b276a6f00ee24ba4bf6bfcf18f7a5272e93
Axel Ribon <axel.ribon@epita.fr>
2019-01-30 20:18:11 +0100

    0.1.0.1
    -Allies Panel Added

---
+++ Assets/Scripts/Menus/AlliesMenu/AlliesMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlliesMenu : MonoBehaviour
+{
+    [SerializeField]
+    GameObject obj;
+
+    //[SerializeField]
+    //prefab
+
+    public void ShowPanel()
+    {
+        obj.SetActive(true);
+    }
+
+    public void HidePanel()
+    {
+        obj.SetActive(false);
+    }
+}
