a4268ad27f3c36379b8465c4916ea4036493944a
Axel Ribon <axel.ribon@epita.fr>
2019-02-07 17:09:10 +0100

    0.1.3.2
    -ADDED : HelpPanel
    -CHANGED : Meat is now food

---
+++ Assets/Scripts/UI/HUD/HelpPanel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class HelpPanel : MonoBehaviour
+{
+    [SerializeField]
+    GameObject obj;
+
+    [SerializeField]
+    Text text;
+
+    public void Call(string text)
+    {
+        obj.SetActive(true);
+        this.text.text = text;
+    }
+
+    public void Hide()
+    {
+        obj.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/UI/HUD/SendHelp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendHelp : MonoBehaviour
+{
+    [TextArea(3, 5)]
+    [SerializeField]
+    string description;
+
+    [SerializeField]
+    HelpPanel panel;
+
+    public void CallHelpPanel()
+    {
+        panel.Call(description);
+    }
+
+    public void Hide()
+    {
+        panel.Hide();
+    }
+}
