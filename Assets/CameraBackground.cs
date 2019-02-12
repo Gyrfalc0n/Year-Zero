c5e1f36069a1f70b9eb40675fa4d5dee501a1c8b
Gyrfalcon <46728024+Gyrfalc0n@users.noreply.github.com>
2019-02-12 09:07:53 +0100

    0.1.14.4
    -Animated background work in progress

---
+++ Assets/CameraBackground.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBackground : MonoBehaviour
+{
+
+    public float speedH = 0.5f;
+    public float speedV = 0.5f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+    }
+}    
+
