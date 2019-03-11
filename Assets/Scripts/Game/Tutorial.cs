76614531806049255b42ec019618cc1376cfefba
Nicola Brankovic <nicola.brankovic@epita.fr>
2019-03-11 18:46:32 +0100

    Commit 0.2.8.4.2

--- Assets/Scripts/Game/Tutorial.cs
+++ Assets/Scripts/Game/Tutorial.cs
@@ -1,34 +1,82 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEditor.Profiling.Memory.Experimental;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Tutorial : MonoBehaviour
 {
+    private bool w8 = false;
     private int step = 0;
     public Text tutoText;
+    public bool runningTutorial = true;
+    [SerializeField] private GameObject TutoPause;
+    [SerializeField] private GameObject Camera;
+    [SerializeField] private GameObject SpaceStationButton;
 
-   
-    void Update()
+
+    void Start()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Event();
-        }
+        Time.timeScale = 0;        
     }
 
-    void Event()
+    void Update()
+    {       
+        if (Input.GetMouseButtonDown(1))
+        {
+            TutoEvent();
+        }
+        if (w8 && InstanceManager.instanceManager.mySelectableObjs.Count == 2)
+        {
+            Time.timeScale = 0;
+            TutoPause.SetActive(true);
+            step++;
+        }
+        //  InstanceManager.instanceManager.mySelectableObjs  pour trouver la liste des objets selectionnables à nous
+        //   SelectUnit.selectUnit.selected
+        //   Time.timeScale = 0;      
+    }
+
+
+    void TutoEvent()
     {
         switch (step)
         {
             case 0:
-                tutoText.text = "We will present you things";
-                break;
+                tutoText.text = "Year Zero is a Real Time Strategy game";
+                step++;
+                break;                
             case 1:
-                tutoText.text = "and others";
+                Camera.GetComponent<CameraController>().LookTo(new Vector3(2f, 0.5f, 2f));
+                tutoText.text = "This is a SpaceStation, it allows you to build workers.";
+                step++;
                 break;
-        }
-
-        step++;
+            case 2:
+                tutoText.text="If you loose all your SpaceStation, the game is over for you !";
+                step++;
+                break;
+            case 3:
+                tutoText.text = "Now Select the SpaceStation and click on the worker button to create one.";
+                w8 = true;
+                TutoPause.SetActive(false);
+                Time.timeScale = 1;
+                w8 = false;
+                break;
+            case 4 :
+                tutoText.text ="Every unit cost a certain amount of resources, and take a certain time to be produced.";
+                step++;
+                break;
+            case 5:
+                tutoText.text = "Good management of these resources is the key (for/to?) victory";
+                step++;
+                break;
+            case 6:
+                tutoText.text = "Now select the worker";
+                step++;
+                break;
+                
+        }           
     }
 }
--- Assets/Scripts/Menus/MainMenu/CampaignMenu.cs
+++ Assets/Scripts/Menus/MainMenu/CampaignMenu.cs
@@ -2,22 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using Photon.Pun;
 public class CampaignMenu : MonoBehaviour
  {
     
      public void StartTuto()
      {
-         SceneManager.LoadScene(4);
-         
+         SceneManager.LoadSceneAsync(4);
+        // PhotonNetwork.LoadLevel(4);
+              
      }
  
      public void StartMission()
      {
          if ((PlayerPrefs.GetInt("tutoCleared",0)==1))
          {
-             SceneManager.LoadScene(5);
-             
+             PhotonNetwork.LoadLevel(5);
+                         
          }
      }
  }
