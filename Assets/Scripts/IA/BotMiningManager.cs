7ae5c6c1acafbb642c45fd9c82ab931b7e6f8994
Axel Ribon <axel.ribon@epita.fr>
2019-04-04 22:50:14 +0200

    0.6.1.0
    -ADDED : Divide miners within the ResourceUnits

--- Assets/Scripts/IA/BotMiningManager.cs
+++ Assets/Scripts/IA/BotMiningManager.cs
@@ -25,4 +25,51 @@ public class BotMiningManager : MonoBehaviour
         }
         return res;
     }
+
+    ResourceUnit GetOptimumResourceUnit(BuilderUnit unit, int index)
+    {
+        return (index == 1) ? GetOptimumAsteroid(unit): GetOptimumFarm(unit);
+    }
+
+    ResourceUnit GetOptimumAsteroid(BuilderUnit unit)
+    {
+        List<ResourceUnit> r = InstanceManager.instanceManager.allResourceUnits;
+        ResourceUnit currentUnit = null;
+        float currentVal = 0;
+
+        for (int i = 0; i < r.Count; i++)
+        {
+            if (r[i].GetResourceIndex() != 1)
+                continue;
+
+            float newVal = (2*(r[i].GetBuildersCount()) + 1) * Vector3.Distance(unit.transform.position, r[i].transform.position);
+            if (newVal < currentVal)
+            {
+                currentUnit = r[i];
+                currentVal = newVal;
+            }
+        }
+        return currentUnit;
+    }
+
+    ResourceUnit GetOptimumFarm(BuilderUnit unit)
+    {
+        List<ResourceUnit> r = InstanceManager.instanceManager.allResourceUnits;
+        ResourceUnit currentUnit = null;
+        float currentVal = 0;
+
+        for (int i = 0; i < r.Count; i++)
+        {
+            if (r[i].GetResourceIndex() != 2)
+                continue;
+
+            float newVal = (3 * (r[i].GetBuildersCount()) + 1) * Vector3.Distance(unit.transform.position, r[i].transform.position);
+            if (newVal < currentVal)
+            {
+                currentUnit = r[i];
+                currentVal = newVal;
+            }
+        }
+        return currentUnit;
+    }
 }
