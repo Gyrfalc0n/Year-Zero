3fa62be2fff44a6c066de61f0476c0d33d22d4f8
Axel Ribon <axel.ribon@epita.fr>
2019-04-19 17:22:51 +0200

    0.6.10.3
    - ADDED : Connection screen is now a little bit dynamic

---
+++ Assets/Scripts/Menus/MainMenu/ConnectionMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConnectionMenu : MonoBehaviour
+{
+    [SerializeField]
+    Text dots;
+    float timer;
+
+    void Start()
+    {
+        timer = 0.3f;
+    }
+
+    void Update()
+    {
+        UpdateDots();
+    }
+
+    void UpdateDots()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            timer = 0.3f;
+            if (dots.text.Length == 5)
+                dots.text = "";
+            else
+                dots.text = new string('.', (dots.text.Length + 1));
+        }
+    }
+}
