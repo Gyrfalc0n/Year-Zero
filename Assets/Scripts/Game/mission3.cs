9b27bccff11e36f834b5295be8dd4d60c1334aa6
Nicola Brankovic <nicola.brankovic@epita.fr>
2019-05-28 16:47:41 +0200

    0.9.8

--- Assets/Scripts/Game/mission3.cs
+++ Assets/Scripts/Game/mission3.cs
@@ -1,3 +1,4 @@
+using System.Timers;
 using Photon.Pun;
 using UnityEngine;
 using UnityEngine.AI;
@@ -10,16 +11,53 @@ public class mission3 : MonoBehaviour
     [SerializeField] public GameObject Background;
 
     private bool isFirstTick = true;
+    private string botPrefab = "IA/BotPrefab";
+    private bool Sucess = false;
+    private int finishtimer = 0;
     
-  
+
+    
+
+
     private string[] voicesToPlay =
     {
-        
-        
+        "VoixMission3.1",
+        "VoixMission3.2",
+        "VoixMission3.3"      
     };     
+    private void Start()
+    {
+        IAManager bot = Instantiate((GameObject)Resources.Load(botPrefab)).GetComponent<IAManager>();
+        bot.gameObject.name = "Bot0";
+        bot.Init(0, 1, 1, 1, new Vector3 (30, 1, -30));
+    }
     void Update()
     {
-                               
+        int timer = (int)InstanceManager.instanceManager.timer;
+        if(timer == 0 && isFirstTick)
+        {
+            isFirstTick = false;
+            FindObjectOfType<AudioManager>().PlaySound(voicesToPlay[0]);            
+        }
+        if (InstanceManager.instanceManager.allSelectableObjs.Count -
+            InstanceManager.instanceManager.mySelectableObjs.Count == 0)
+        {
+            Sucess = true;
+            FindObjectOfType<AudioManager>().PlaySound(voicesToPlay[1]);
+            finishtimer = timer;
+        }
+        if (Sucess && timer == finishtimer + 25)
+        {
+            EndMission();
+        }
+        
     }
+    void EndMission()
+    {
+        PlayerPrefs.SetInt("mission3Cleared",1);
+        PhotonNetwork.LoadLevel("MainMenu");
+        FindObjectOfType<AudioManager>().PlaySound("MainMenuMusic");
+    }   
+
  
 }
